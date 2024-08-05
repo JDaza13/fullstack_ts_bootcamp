9b4609a73ade9d48a1dc6c097b7a65d75eff09be
JDaza13 <jdaza1327@gmail.com>
2024-08-04 19:04:41 -0500

    chore: obfuscate-sourcemaps plugin

---
+++ vite-plugins/vite-plugin-obfuscate-sourcemaps.ts
@@ -0,0 +1,38 @@
+import crypto from "crypto";
+import * as fs from "fs";
+import path from "path";
+import type { Plugin } from "vite";
+
+/**
+ * Generates an obfuscated filename by inserting an crypto-generated string in the original filename
+ * @param source the original filename
+ * @returns the obfuscate filename string
+ */
+const generateObfuscatedFilename = (source: string): string => {
+  const randomString = crypto.randomBytes(8).toString("hex");
+  return source.replace(/\.js.map$/, `-${randomString}.js.map`);
+};
+
+/**
+ * A vite (rollup) plugin that generate obsfucated sourcemaps
+ * @param assetsDir path to the assets dir
+ * @returns Plugin
+ */
+export default function myPlugin(assetsDir: string): Plugin {
+  return {
+    name: "obfuscate-sourcemaps",
+    async closeBundle() {
+      const assetsPath = path.resolve(assetsDir);
+      fs.readdir(assetsPath, (err, files) => {
+        if (err) throw err;
+        files.forEach((file) => {
+          if (file.endsWith(".map")) {
+            const source = path.join(assetsDir, file);
+            const resultSourcemapFilename = generateObfuscatedFilename(source);
+            fs.copyFileSync(source, resultSourcemapFilename);
+          }
+        });
+      });
+    },
+  };
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import express from "vite3-plugin-express";
+import obfuscateSourcemaps from "./vite-plugins/vite-plugin-obfuscate-sourcemaps";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), express("src/server")],
+  plugins: [react(), obfuscateSourcemaps("dist/assets"), express("src/server")],
+  build: {
+    sourcemap: true,
+    chunkSizeWarningLimit: 1024 * 1024 * 1,
+  },
 });
